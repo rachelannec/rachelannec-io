2c01972d655ebd9a94968af93842b174df47996d
Rachel Anne Cilon <rachelannecilon@gmail.com>
2024-12-04 13:34:00 +0800

    most are done, except the project tab :<

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
-document.getElementById('project-link').addEventListener('click', function(event) {
+document.getElementById('projectLink').addEventListener('click', function(event) {
     event.preventDefault(); // Prevents the default action of the anchor tag (navigation)
-    alert('There is nothing yet. 😔 (You may see my Github instead hehe)');
+    alert('There is nothing here yet. 😔\n(You may checnk my Github profile instead hehe)');
   });
 
   
