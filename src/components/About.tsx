dcb377564cda102500edf998992ab6ba9b764268
Rachel Anne Cilon <annerachel095@gmail.com>
2026-02-10 20:28:46 +0800

    update about me waahha

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,9 +5,9 @@ const About = (): React.JSX.Element => {
     <section className="my-4">
       <h2>About Me</h2>
       <p className="text-justify">
-        Hello, I'm Rachel. Currently a freshman at the Polytechnic University of the Philippines taking BS Computer Science.
-        Been learning Web Development, Machine Learning, and Data Science through student orgs' bootcamps, workshops, and stuff. 
-        It might be about 3 years now since I start learning to code, but I'm still not very skilled at it. I think it's because I'm so focused on my academics during my senior high days. 👾 <br />
+        Hello, I'm Rachel. Currently a sophomore at the Polytechnic University of the Philippines taking BS Computer Science.
+        Been learning Web Development. 
+        Just want to pass my academics. 👾 <br />
         That is why let us keep on learning more and more. 📚 <br />
       </p>
     </section>
